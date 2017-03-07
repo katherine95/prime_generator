3cd6ad39490ee5ce1947a4f0406d2c245be0fac9
katherine95 <kathiekim95@gmail.com>
2017-03-07 12:06:59 +0300

    generating primes by eliminating even numbers

--- primegen.py
+++ primegen.py
@@ -1,6 +1,12 @@
-def prime():
-    for i in range(1, n):
+def prime(n):
+    n = abs(int(n))
+    if n<2:
+        return False
+    if n==2:
+        return True
+    if not n & 1:
+        return False
+    for i in range(3, int(n**0.5)+1, 2):
         if n % i == 0:
             return False
-    return True
-print(prime(7))            
+        return True    
--- test_primegen.py
+++ test_primegen.py
@@ -1,17 +1,17 @@
 import unittest
 from primegen import prime
 class Testprimegen(unittest.TestCase):
-    def test_number_equal_zero(self):
-        self.assertFalse(prime(0))
+    def test_number_is_positive(self):
+        self.assertFalse(prime(-1))
+    def test_number_equal_two(self):
+        self.assertTrue(prime(2))
+    def test_number_isnot_two_and_one(self):
+        self.assertTrue(prime(2))
     def test_number_equal_one(self):
         self.assertFalse(prime(1))
-    def test_number_equal_two(self):
-        self.assertFalse(prime(2))
-    def test_number_less_than_two(self):
-        self.assertFalse(prime(-1))
     def test_number_modulus_i_equal_zero(self):
         self.assertFalse(prime(10))
-    def test_number_modulus_i_notequal_zero(self):
+    def test_number_modulus_i_not_equal_zero(self):
         self.assertTrue(prime(11))
 
 if __name__=="__main__":
